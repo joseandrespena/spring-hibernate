d0bcf1c4dd5922b931d152c2b0e469b090d96e9e
Jose Andres Pena <joandp@gmail.com>
2010-06-20 21:07:19 -0600

    Added Base class for integration tests

---
+++ src/test/java/com/yxz97/hr/service/BaseServiceIntegrationTest.java
@@ -0,0 +1,21 @@
+/******************************************************************
+  			BaseServiceTest.java 
+			- Attribution-Noncommercial-Share Alike 3.0 Unported
+
+You are free, to Share  — to copy, distribute and transmit the work, 
+to Remix — to adapt the work refer to 
+http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US more details
+
+********************************************************************/
+
+package com.yxz97.hr.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:C:\\Users\\jose\\Documents\\Developer\\spring-hibernate\\src\\main\\webapp\\WEB-INF\\applicationContext.xml")
+public class BaseServiceIntegrationTest {
+
+}
