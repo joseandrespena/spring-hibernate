73cece07d19adc73943b0e7cd2d3479dada026aa
joseandrespena <joandp@gmail.com>
2010-07-16 09:39:02 -0600

    Added properties for more attributes of the model object

--- src/main/java/com/yxz97/hr/domain/Person.java
+++ src/main/java/com/yxz97/hr/domain/Person.java
@@ -12,13 +12,16 @@ package com.yxz97.hr.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Type;
+
 @Entity
 @Table(name = "Person")
-@NamedQueries( {
+@NamedQueries({
 		@NamedQuery(name = "getPersonByName", query = "from Person m where m.firstName LIKE :name"),
 		@NamedQuery(name = "listPersonByName", query = "from Person m order by m.lastName, firstName") })
 public class Person extends BaseDomain {
@@ -26,19 +29,112 @@ public class Person extends BaseDomain {
 	private static final long serialVersionUID = 1L;
 
 	@Column(name = "firstName")
-	private String firstName;
+	String firstName;
 
 	@Column(name = "lastName")
-	private String lastName;
+	String lastName;
 
 	@Column(name = "email")
-	private String email;
+	String email;
 
 	@Column(name = "address")
-	private String address;
+	String address;
 
 	@Column(name = "phone")
-	private String phone;
+	String phone;
+
+	@Column(length = 1, nullable = false)
+	Short gender;
+
+	String birthDate;
+
+	@JoinColumn(columnDefinition = "position_id", name = "position_id")
+	@Type(type = "com.yxz97.hr.domain.Person")
+	Position position;
+
+	String office;
+
+	String mobile;
+
+	String username;
+
+	String password;
+
+	String keywords;
+
+	String photo;
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public Short getGender() {
+		return gender;
+	}
+
+	public void setGender(Short gender) {
+		this.gender = gender;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getOffice() {
+		return office;
+	}
+
+	public void setOffice(String office) {
+		this.office = office;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
 
 	public String getFullName() {
 		return this.firstName + " " + this.lastName;
