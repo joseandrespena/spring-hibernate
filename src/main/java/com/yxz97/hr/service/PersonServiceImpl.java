e4363e09155973b4c7205d1c14b61a9cf831f8f1
joseandrespena <joandp@gmail.com>
2010-08-05 09:59:04 -0600

    Moved the message broker, to a spring based flex official integration

--- src/main/java/com/yxz97/hr/service/PersonServiceImpl.java
+++ src/main/java/com/yxz97/hr/service/PersonServiceImpl.java
@@ -14,18 +14,14 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.springframework.flex.remoting.RemotingDestination;
-import org.springframework.flex.remoting.RemotingExclude;
-import org.springframework.flex.remoting.RemotingInclude;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.yxz97.hr.dao.PersonDao;
 import com.yxz97.hr.domain.Person;
 
-@Service
+@Service("personService")
 @Transactional(propagation=org.springframework.transaction.annotation.Propagation.REQUIRED, readOnly=true)
-@RemotingDestination
 public class PersonServiceImpl implements PersonService {
 	
 	@Resource
@@ -33,49 +29,41 @@ public class PersonServiceImpl implements PersonService {
 
 	@Override
 	@Transactional(readOnly=false)	
-	@RemotingInclude
 	public void delete(Person entity) {
 		personDao.delete(entity);
 	}
 
 	@Override
 	@Transactional(readOnly=false)
-	@RemotingInclude
 	public void delete(Long entityId) {
 		personDao.delete(entityId);
 	}
 
 	@Override
-	@RemotingInclude
 	public List<Person> loadAll() {
 		return personDao.loadAll();		
 	}
 
 	@Override
-	@RemotingInclude
 	public Person loadById(Long entityId) {
 		return personDao.loadById(entityId);
 	}
 
 	@Override
-	@RemotingInclude
 	public Person read(String entityName) {		
 		return personDao.read(entityName);
 	}
 
 	@Override
 	@Transactional(readOnly=false)
-	@RemotingInclude
 	public Person save(Person entity) {
 		return personDao.save(entity);
 	}
 
-	@RemotingExclude
 	public void setPersonDao(PersonDao personDao) {
 		this.personDao = personDao;
 	}
 
-	@RemotingExclude
 	public PersonDao getPersonDao() {
 		return personDao;
 	}
